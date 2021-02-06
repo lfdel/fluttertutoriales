3bdefd23d505412920b16755b88a874ed982fe6c
Leonardo <lfdel24@gmail.com>
2021-02-06 15:33:04 -0500

    Terminada la primera parte de diseño.

--- lib/tutorial_de_layout.dart
+++ lib/tutorial_de_layout.dart
@@ -3,15 +3,41 @@ import 'package:flutter/material.dart';
 class TutorialDeLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
     return Scaffold(
       appBar: AppBar(title: Text('Flutter layout demo')),
-      body: Column(
-        children: [
-          _titleSection,
-        ],
+      body: Container(
+        child: ListView(
+          children: [
+            Image.asset(
+              'lake.jpg',
+              width: double.infinity,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            _titleSection,
+            _buttonSecction(color),
+            _textSecction,
+          ],
+        ),
       ),
     );
   }
+
+  Widget _textSecction = Container(
+    padding: const EdgeInsets.all(32),
+    child: Text(
+      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+      'Alps. Situated 1,578 meters above sea level, it is one of the '
+      'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+      'half-hour walk through pastures and pine forest, leads you to the '
+      'lake, which warms to 20 degrees Celsius in the summer. Activities '
+      'enjoyed here include rowing, and riding the summer toboggan run.',
+      softWrap: true,
+      textAlign: TextAlign.justify,
+    ),
+  );
 
   Widget _titleSection = Container(
     padding: const EdgeInsets.all(32),
@@ -41,4 +67,41 @@ class TutorialDeLayout extends StatelessWidget {
       ],
     ),
   );
+
+  Widget _buttonSecction(Color color) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          color: color,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
+    );
+  }
 }
