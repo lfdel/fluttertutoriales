8a97979593f95dc6b6c8888255d44f13a2e61889
Leonardo <lfdel24@gmail.com>
2021-02-07 21:09:21 -0500

    Añade interactividad

--- lib/tutorial_de_layout.dart
+++ lib/tutorial_de_layout.dart
@@ -58,11 +58,7 @@ class TutorialDeLayout extends StatelessWidget {
             )
           ],
         )),
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41')
+        _FavoriteWidget()
       ],
     ),
   );
@@ -102,5 +98,50 @@ class TutorialDeLayout extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class _FavoriteWidget extends StatefulWidget {
+  @override
+  __FavoriteWidgetState createState() => __FavoriteWidgetState();
+}
+
+class __FavoriteWidgetState extends State<_FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
   }
 }
